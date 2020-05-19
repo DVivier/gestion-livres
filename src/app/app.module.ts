dbdb9b29e2c8c11da6ed6ec6b72725a6f1c4431c
damien vivier <vivdams@gmail.com>
2020-05-19 15:28:30 +0200

    draft : addBook, ajout controls issus de API Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';  
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule }     from '@angular/material/datepicker';
+// import { MatNativeDateModule }     from '@angular/material/d';
+
+  
+import { MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -43,11 +52,17 @@ const appRoutes: Routes = [
     SignInComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+      BrowserModule,
+      MatButtonModule,
+      MatCheckboxModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatSelectModule,
+      AppRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule
   ],
   providers: [
      AuthGuardService,
--- src/app/bookshelf/book-form/book-form.component.ts
+++ src/app/bookshelf/book-form/book-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl,  } from '@angular/forms';
 import { Router } from '@angular/router';
-// import { BooksService } from 'src/app/services/books.service';
 import { BooksService } from '../../services/books.service';
 
 @Component({
@@ -12,21 +11,55 @@ import { BooksService } from '../../services/books.service';
 
 export class BookFormComponent implements OnInit {
    
-   constructor(private router:Router, private bookService:BooksService ) { }
+   constructor(private router:Router, 
+               private bookService:BooksService,
+               private formBuilder:FormBuilder ) { }
    
+   bookForm : FormGroup;
+
+   bookFormMessages = {
+      'nombreDePages' : [
+         {type: 'required', message : 'Le nombre de pages est obligatoire'},
+         {type: 'min', message : 'Le livre doit comporter au moins 10 pages'},
+         {type: 'pattern', message : 'Ce champ doit être numérique'}
+      ],
+      'annee' : [
+         {type: 'required', message : "L'année est obligatoire"},
+         {type: 'pattern', message : "L'année doit être sur 4 chiffres"}
+      ]
+   }
+
    ngOnInit(): void {
+      this.initForm();
    }
    
+   initForm(){
+
+      this.bookForm = this.formBuilder.group({
+         titre : ['', Validators.required],
+         auteur : ['', Validators.required],
+         annee : new FormControl('', Validators.compose([
+            Validators.pattern('[0-9]{4}'),
+            Validators.required
+         ])),
+         nombreDePages : new FormControl('', Validators.compose([
+            Validators.min(10),
+            Validators.pattern('[0-9]*'),
+            Validators.required
+         ]))
+      });
+   }
    
-   onSubmit(form: NgForm) {
-      console.log(form.value);
+   onSubmit() {
+      // console.log(form.value);
+      console.log(this.bookForm.value);
       
-      const titre = form.value['titre'];
-      const auteur = form.value['auteur'];
-      const annee = form.value['annee'];
-      const nombreDePages = form.value['nombreDePages'];
+      const titre = this.bookForm.get('titre').value;
+      const auteur = this.bookForm.get('auteur').value;
+      const annee = this.bookForm.get('annee').value;
+      const nombreDePages = this.bookForm.get('nombreDePages').value;
       
-      this.bookService.addBook(titre, auteur, annee,nombreDePages);
+      this.bookService.addBook(titre, auteur, annee, nombreDePages);
       this.router.navigate(['/bookshelf']);
    }
 }
